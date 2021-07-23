c1dbc8055132a861571cfb44411ca66848d41dd9
praveen <gbpraveen55@gmail.com>
2021-07-23 09:37:26 +0530

    myproject

---
+++ New folder/SimpleWebApp/src/test/java/WAR/AppTest.java
@@ -0,0 +1,20 @@
+package WAR;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
